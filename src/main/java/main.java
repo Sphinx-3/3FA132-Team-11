74365a5abc55000438c9276e99294685a640828a
Sphinx-3 <finn.balint@gmail.com>
2024-12-15 22:46:57 +0100

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class main {
+
+    public static void main(String[] args) {
+
+
+
+        Connection con = DriverManager.getConnection()
+    }
+}
